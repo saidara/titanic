e1f0853988262711d9066ef3326c2066c4704e17
saidara <64938726+saidara@users.noreply.github.com>
2022-03-03 00:19:46 -0500

    Add files via upload

---
+++ titanic/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request,render_template,jsonify
+import joblib
+import os
+import numpy as np
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+@app.route('/', methods=['POST', 'GET'])
+def result():
+    ticket_class = float(request.form['class'])
+    gender = float(request.form['gender'])
+    age = float(request.form['age'])
+    siblings = float(request.form['siblings'])
+    parents = float(request.form['parents'])
+    fair = float(request.form['fair'])
+    onboard = float(request.form['onboard'])
+
+
+
+    X = np.array([ticket_class,gender,age,siblings,parents,fair,onboard]).reshape(1,-1)
+
+    print(X)
+
+
+    model_path = r'C:\Users\shanm\OneDrive\Desktop\titanic\model\rf.sav'
+
+    model = joblib.load(model_path)
+
+    Y = model.predict(X)
+    pred = "survived" if Y[0]==1 else "died"
+
+    return render_template('index.html', prediction_text = '{}'.format(pred))
+
+
+if __name__ =="__main__":
+    app.run(debug=True, port=5623)
